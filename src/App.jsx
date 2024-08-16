91a6295e96ea0dfbe9f3937677e20e129ce262c4
unknown <zviadbarbakadze5@gmail.com>
2024-08-16 10:19:40 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awesome Blog </h1>
+      <div id="page-body">Welcome to my blog!</div>
+    </div>
+  );
+}
+
+export default App;
