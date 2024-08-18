11dc30c3c3b067ab84972b9b0318ba86292e7d58
unknown <zviadbarbakadze5@gmail.com>
2024-08-18 15:52:18 +0400

    last changes on weekends to the front

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,8 +1,23 @@
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import axios from 'axios'
+import { useEffect, useState } from "react";
 const ArticlePage = () => {
+  const[articleInfo,setArticleInfo]=useState({upvotes:0,comments:[]})
   const { articleId } = useParams();
+  useEffect( ()=>{
+    async()=>{
+      const response= await axios.get(`/api/articles/${articleId}`)
+      .then(()=>{
+        console.log(response.data)
+        setArticleInfo(response.data)
+      })
+     
+    }
+    
+  },[])
+  
   const article = articles.find((article) => article.name === articleId);
   if (!article) {
     return <NotFoundPage />;
@@ -10,7 +25,7 @@ const ArticlePage = () => {
 
   return (
     <div>
-      <h1>{article.title}</h1>
+      <h1>{articleInfo.comments[0]}</h1>
       {article.content.map((content) => (
         // eslint-disable-next-line react/jsx-key
         <p>{content}</p>
